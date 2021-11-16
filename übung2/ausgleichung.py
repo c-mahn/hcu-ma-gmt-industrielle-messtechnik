5bfc03a9d21728bcc6dc99aa7e9084edf18834b1
cmahn <mail@cmahn.de>
2021-11-16 17:09:10 +0100

    Update ausgleichung.py

--- übung2/ausgleichung.py
+++ übung2/ausgleichung.py
@@ -16,6 +16,13 @@
 # import string as st
 # import random as r
 import numpy as np
+import os
+
+
+# -----------------------------------------------------------------------------
+# Debugging-Settings
+
+verbose = False  # Shows more debugging information
 
 
 # Functions
@@ -30,4 +37,34 @@ import numpy as np
 # -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    pass
+    
+    # Aufstellen des x-Vektors
+    x_vektor = np.array([["h2"],
+                         ["h3"],
+                         ["h4"],
+                         ["h5"]])
+
+    # Aufstellen des Beobachtungsvektors
+    l_vektor = np.array([[h_12],
+                         [h_23],
+                         [h_34],
+                         [h_45],
+                         [h_15]])
+    if(verbose):
+        print(f"l_vektor:\n{l_vektor}\n")
+
+    # Aufstellen der A-Matrix
+    a_matrix = np.zeros([len(l_vektor), len(x_vektor)])
+    a_matrix[0][0] = 1
+    a_matrix[1][0] = -1
+    a_matrix[1][1] = 1
+    a_matrix[2][1] = -1
+    a_matrix[2][2] = 1
+    a_matrix[3][2] = -1
+    a_matrix[3][3] = 1
+    a_matrix[4][3] = 1
+    if(verbose):
+        print(f"a_matrix:\n{a_matrix}\n")
+    
+    
+    
