b47ac66b7e97e56c65b88e3525c9248cccf65781
cmahn <mail@cmahn.de>
2022-01-19 21:59:20 +0100

    Added totalstation-convert

--- übung3/totalstation-convert.py
+++ übung3/totalstation-convert.py
@@ -1,4 +1,4 @@
-# Überprürfung der Rechtwinkligkeit
+# Parsen und konvertieren von Tachymeter-Daten
 # #############################################################################
 
 # Authors:
@@ -37,22 +37,12 @@ verbose = True  # Shows more debugging information
 # -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    
-    # Import der Messwerte des Neigungssensors
-    file = open(os.path.join("data","imu_data_converted.txt"))
-    imu_data = file.readlines()
-    file.close()
-    for i, e in enumerate(imu_data):
-        imu_data[i] = e.strip().split(";")
-        temp = []
-        for j in imu_data[i]:
-            temp.append(float(j))
-        imu_data[i] = temp
 
     # Import der Messwerte des Tachymeters
-    file = open(os.path.join("data","totalstation_data_converted.txt"))
+    file = open(os.path.join("data","totalstation_data.txt"))
     totalstation_data = file.readlines()
     file.close()
+    totalstation_data.pop(0)
     for i, e in enumerate(totalstation_data):
         totalstation_data[i] = e.strip().split(";")
         temp = []
@@ -63,5 +53,17 @@ if __name__ == '__main__':
                 temp.append(f)
         totalstation_data[i] = temp
 
-    for i in imu_data[0:3]:
-        print(f"{i[8]}, {i[9]}, {i[10]}")
+    for i, e in enumerate(totalstation_data):
+        totalstation_data[i][3] = e[3] /200*np.pi
+        totalstation_data[i][4] = e[4] /200*np.pi
+
+    # Export
+    file = open(os.path.join("data","totalstation_data_converted.txt"),f"w")
+    for i in totalstation_data:
+        for j, e in enumerate(i):
+            if(j == 0):
+                file.writelines(f"{e}")
+            else:
+                file.writelines(f"; {e}")
+        file.writelines(f"\n")
+    file.close()
