1edba22b25c6260c2ea45cb1cc2dd99de599dcc9
cmahn <mail@cmahn.de>
2022-01-19 21:05:29 +0100

    Finished IMU-Conversion

--- übung3/imu-convert.py
+++ übung3/imu-convert.py
@@ -50,26 +50,39 @@ if __name__ == '__main__':
             data_split.append(e.strip().split(" "))
 
     # Select only marked lines
-    markers = []
+    raw_markers = []
     for i in data_split:
         if(i[0] != "0000"):
-            markers.append(i)
+            raw_markers.append(i)
 
     # Convert lines in float-lists
-    raw_markers = []
-    for i, e in enumerate(markers):
-        raw_markers.append([])
+    markers = []
+    for i, e in enumerate(raw_markers):
+        markers.append([])
         for j in e:
             if(j != ""):  # Skip empty strings
-                raw_markers[i].append(float(j))
-        print(raw_markers[i])
+                markers[i].append(float(j))
 
     # Convert units
-    
-    
+    for i, e in enumerate(markers):
+        if(len(e) == 12):
+            markers[i][0] = int(e[0]) # marker-id to integer
+            markers[i][1] = e[1]
+            markers[i][2] = e[2] /180*np.pi  # convert °/s to rad/s
+            markers[i][3] = e[3] /180*np.pi  # convert °/s to rad/s
+            markers[i][4] = e[4] /180*np.pi  # convert °/s to rad/s
+            markers[i][5] = e[5]
+            markers[i][6] = e[6]
+            markers[i][7] = e[7]
+            markers[i][8] = e[8] /180*np.pi  # convert ° to s
+            markers[i][9] = e[9] /180*np.pi  # convert ° to s
+            markers[i][10] = e[10] /180*np.pi  # convert ° to s
+            markers[i][11] = e[11] -273.15  # convert °K to °C
 
     # Export
     file = open(os.path.join("data","imu_data_converted.txt"),f"w")
     for i in markers:
-        file.writelines(f"{i}\n")
+        for j in i:
+            file.writelines(f"{j}; ")
+        file.writelines(f"\b\b\n")
     file.close()
