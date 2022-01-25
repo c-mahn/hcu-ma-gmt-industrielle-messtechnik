e023e75dba37d69b8e5e766edb6cbefa27a279a8
cmahn <mail@cmahn.de>
2022-01-25 23:38:24 +0100

    Create schwingungsanalyse.py

---
+++ übung4/schwingungsanalyse.py
@@ -0,0 +1,34 @@
+# Schwingungsanalyse
+# #############################################################################
+
+# This python script calculates the frequency of the occilation.
+
+# Authors:
+# Joshua Wolf
+# Silas Teske
+# Lasse Zeh
+# Christopher Mahn
+
+# #############################################################################
+
+# Import of Libraries
+# -----------------------------------------------------------------------------
+
+# import math as m
+# import string as st
+# import random as r
+# import re
+import os
+
+# Functions
+# -----------------------------------------------------------------------------
+
+
+# Classes
+# -----------------------------------------------------------------------------
+
+# Beginning of the Programm
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    pass
