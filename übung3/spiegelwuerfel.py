4ca0b08dab2d993244b5b4a065c598033553f29a
cmahn <mail@cmahn.de>
2022-01-25 19:25:10 +0100

    Changed commandline output

--- übung3/spiegelwuerfel.py
+++ übung3/spiegelwuerfel.py
@@ -69,59 +69,78 @@ if __name__ == '__main__':
     ori1IMU.append(np.average([imu_data[0][8], imu_data[1][8], imu_data[2][8]]))
     ori1IMU.append(np.average([imu_data[0][9], imu_data[1][9], imu_data[2][9]]))
     ori1IMU.append(np.average([imu_data[0][10], imu_data[1][10], imu_data[2][10]]))
+    if(verbose):
+        print(f"[Debug] ori1IMU: {ori1IMU}")
 
     ori2IMU = []
     ori2IMU.append(np.average([imu_data[7][8], imu_data[8][8], imu_data[9][8]]))
     ori2IMU.append(np.average([imu_data[7][9], imu_data[8][9], imu_data[9][9]]))
     ori2IMU.append(np.average([imu_data[7][10], imu_data[8][10], imu_data[9][10]]))
+    if(verbose):
+        print(f"[Debug] ori2IMU: {ori2IMU}")
 
     avg_imu1 = []
     avg_imu1.append(np.average([totalstation_data[1][3],totalstation_data[3][3],totalstation_data[5][3]]))
     avg_imu1.append(np.average([totalstation_data[1][4],totalstation_data[3][4],totalstation_data[5][4]]))
+    if(verbose):
+        print(f"[Debug] avg_imu1: {avg_imu1}")
 
     avg_imu2 = []
     avg_imu2.append(np.average([totalstation_data[7][3],totalstation_data[9][3],totalstation_data[11][3]]))
     avg_imu2.append(np.average([totalstation_data[7][4],totalstation_data[9][4],totalstation_data[11][4]]))
+    if(verbose):
+        print(f"[Debug] avg_imu2: {avg_imu2}")
 
     avg_mirror1 = []
     avg_mirror1.append(np.average([totalstation_data[0][3],totalstation_data[2][3],totalstation_data[4][3]]))
     avg_mirror1.append(np.average([totalstation_data[0][4],totalstation_data[2][4],totalstation_data[4][4]]))
+    if(verbose):
+        print(f"[Debug] avg_mirror1: {avg_mirror1}")
     
     avg_mirror2 = []
     avg_mirror2.append(np.average([totalstation_data[6][3],totalstation_data[8][3],totalstation_data[10][3]]))
     avg_mirror2.append(np.average([totalstation_data[6][4],totalstation_data[8][4],totalstation_data[10][4]]))
+    if(verbose):
+        print(f"[Debug] avg_mirror2: {avg_mirror2}")
 
     ori1mirror = []
     ori1mirror.append(ori1IMU[0])                               # roll
     ori1mirror.append((avg_imu1[1]-avg_mirror1[1])+ori1IMU[1])  # pitch
     ori1mirror.append((avg_imu1[0]-avg_mirror1[0])+ori1IMU[2])  # yaw
+    if(verbose):
+        print(f"[Debug] ori1mirror: {ori1mirror}")
 
     ori2mirror = []
     ori2mirror.append(ori2IMU[0])                               # roll
     ori2mirror.append((avg_imu2[1]-avg_mirror2[1])+ori2IMU[1])  # pitch
     ori2mirror.append((avg_imu2[0]-avg_mirror2[0])+ori2IMU[2])  # yaw
+    if(verbose):
+        print(f"[Debug] ori2mirror: {ori2mirror}")
 
     difference = []
     difference.append(ori2mirror[0]-ori1mirror[0])
     difference.append(ori2mirror[1]-ori1mirror[1])
     difference.append(ori2mirror[2]-ori1mirror[2])
+    if(verbose):
+        print(f"[Debug] difference: {difference}")
 
     differencegon = []
     differencegon.append(difference[0]*(200/np.pi))
     differencegon.append(difference[1]*(200/np.pi))
     differencegon.append(difference[2]*(200/np.pi))
-
-    print(differencegon)
+    if(verbose):
+        print(f"[Debug] differencegon: {differencegon}")
 
     # Berechnungskontrolle
     innenwinkel_imu = -(ori1IMU[2] - ori2IMU[2])
-    print(f"Innenwinkel (IMU): {(innenwinkel_imu/np.pi*200):.5f} gon")
     drehung_totalstation1 = avg_mirror1[0] - avg_imu1[0]
-    print(f"Innenwinkel (Totalstation1): {(drehung_totalstation1/np.pi*200):.5f} gon")
     drehung_totalstation2 = avg_imu2[0] - avg_mirror2[0]
-    print(f"Innenwinkel (Totalstation2): {(drehung_totalstation2/np.pi*200):.5f} gon")
     innenwinkel = 2*np.pi - innenwinkel_imu - drehung_totalstation1 - drehung_totalstation2
-    print(f"Innenwinkel: {(innenwinkel/np.pi*200):.5f} gon")
+    if(verbose):
+        print(f"[Debug] Innenwinkel (IMU): {(innenwinkel_imu/np.pi*200):.5f} gon")
+        print(f"[Debug] Innenwinkel (Totalstation1): {(drehung_totalstation1/np.pi*200):.5f} gon")
+        print(f"[Debug] Innenwinkel (Totalstation2): {(drehung_totalstation2/np.pi*200):.5f} gon")
+        print(f"[Debug] Innenwinkel: {(innenwinkel/np.pi*200):.5f} gon")
 
     # Export results
     file = open(os.path.join("data","results.txt"),f"w")
