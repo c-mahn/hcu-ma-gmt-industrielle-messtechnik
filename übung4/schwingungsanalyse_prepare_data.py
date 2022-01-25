a92fecce47e87ef56bd2cd012d6c6328f2b1f287
cmahn <mail@cmahn.de>
2022-01-26 00:00:24 +0100

    Added a bunch of files and python code

--- übung4/schwingungsanalyse_prepare_data.py
+++ übung4/schwingungsanalyse_prepare_data.py
@@ -21,6 +21,13 @@
 # import re
 import os
 
+
+# -----------------------------------------------------------------------------
+# Debugging-Settings
+
+verbose = True  # Shows more debugging information
+
+
 # Functions
 # -----------------------------------------------------------------------------
 
@@ -34,4 +41,29 @@ import os
 
 if __name__ == '__main__':
     print("Runnung schwingungsanalyse_prepare_data.py...")
-    
+
+    # Import der Messwerte von "Schwingungsanalyse_50Hz_PtoP_0.1.txt"
+    file = open(os.path.join("data","Schwingungsanalyse_50Hz_PtoP_0.1.txt"))
+    data = file.readlines()
+    file.close()
+    data.pop(0)
+    for i, e in enumerate(data):
+        data[i] = e.strip().split(",")
+        temp = []
+        for j, f in enumerate(data[i]):
+            if(j == 0):
+                temp.append(int(f))
+            else:
+                temp.append(float(f))
+        data[i] = temp
+
+    # Export der konvertierten Messwerte von "Schwingungsanalyse_50Hz_PtoP_0.1.txt"
+    file = open(os.path.join("data","totalstation_data_converted.txt"),f"w")
+    for i in totalstation_data:
+        for j, e in enumerate(i):
+            if(j == 0):
+                file.writelines(f"{e}")
+            else:
+                file.writelines(f"; {e}")
+        file.writelines(f"\n")
+    file.close()
